3a73229c1aa65c7a93d8daf017e334e2fdaa7590
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-19 17:45:05 -0400

    Add Quotes UI template in quotes.blade.php

---
+++ resources/views/quotes.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Quotes UI</title>
+    <link rel="stylesheet" href="{{ asset('vendor/quotes/assets/app.css') }}" />
+</head>
+<body>
+    <div id="app"></div>
+    <script src="{{ asset('vendor/quotes/assets/app.js') }}"></script>
+</body>
+</html>
