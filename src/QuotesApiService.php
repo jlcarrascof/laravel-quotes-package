ddacc7b5661af2105f638cd3d3d579e360007830
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-19 13:28:22 -0400

    Implement binary search for cached quotes in QuotesApiService

--- src/QuotesApiService.php
+++ src/QuotesApiService.php
@@ -2,6 +2,7 @@
 namespace Jjmartinezf\Quotes;
 
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\RateLimiter;
 
 class QuotesApiService
 {
@@ -43,13 +44,40 @@ class QuotesApiService
 
     public function getQuote($id)
     {
+        $cachedQuote = $this->binarySearch($id);
+        if ($cachedQuote) {
+            return $cachedQuote;
+        }
+
         return $this->attemptRequest("quotes:{$id}", function () use ($id) {
             $response = Http::get("{$this->baseUrl}/quotes/{$id}");
             $quote = $response->successful() ? $response->json() : null;
             if ($quote) {
                 $this->cache[] = $quote;
+                usort($this->cache, fn($a, $b) => $a['id'] <=> $b['id']);
             }
             return $quote;
         });
+    }
+
+    private function binarySearch($id)
+    {
+        $left = 0;
+        $right = count($this->cache) - 1;
+
+        while ($left <= $right) {
+            $mid = floor(($left + $right) / 2);
+            $quoteId = $this->cache[$mid]['id'] ?? null;
+
+            if ($quoteId == $id) {
+                return $this->cache[$mid];
+            }
+            if ($quoteId < $id) {
+                $left = $mid + 1;
+            } else {
+                $right = $mid - 1;
+            }
+        }
+        return null;
     }
 }
