b8b5da61cf1e2111e984e9d1d2501e9e237457f8
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-19 13:27:14 -0400

    Refactor QuotesApiService to implement rate limiting for API requests

--- src/QuotesApiService.php
+++ src/QuotesApiService.php
@@ -13,27 +13,43 @@ class QuotesApiService
         $this->baseUrl = config('quotes.api_url');
     }
 
+    protected function attemptRequest($key, callable $callback)
+    {
+        return RateLimiter::attempt(
+            $key,
+            config('quotes.rate_limit', 100),
+            $callback,
+            config('quotes.rate_window', 60)
+        );
+    }
+
     public function getAllQuotes()
     {
-        $response = Http::get("{$this->baseUrl}/quotes");
-        $quotes = $response->successful() ? $response->json()['quotes'] : [];
-        $this->cache = $quotes;
-        return $quotes;
+        return $this->attemptRequest('quotes:all', function () {
+            $response = Http::get("{$this->baseUrl}/quotes");
+            $quotes = $response->successful() ? $response->json()['quotes'] : [];
+            $this->cache = $quotes;
+            return $quotes;
+        });
     }
 
     public function getRandomQuote()
     {
-        $response = Http::get("{$this->baseUrl}/quotes/random");
-        return $response->successful() ? $response->json() : null;
+        return $this->attemptRequest('quotes:random', function () {
+            $response = Http::get("{$this->baseUrl}/quotes/random");
+            return $response->successful() ? $response->json() : null;
+        });
     }
 
     public function getQuote($id)
     {
-        $response = Http::get("{$this->baseUrl}/quotes/{$id}");
-        $quote = $response->successful() ? $response->json() : null;
-        if ($quote) {
-            $this->cache[] = $quote;
-        }
-        return $quote;
+        return $this->attemptRequest("quotes:{$id}", function () use ($id) {
+            $response = Http::get("{$this->baseUrl}/quotes/{$id}");
+            $quote = $response->successful() ? $response->json() : null;
+            if ($quote) {
+                $this->cache[] = $quote;
+            }
+            return $quote;
+        });
     }
 }
