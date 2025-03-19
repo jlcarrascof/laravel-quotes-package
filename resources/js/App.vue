bec94b66cc51777314715be1b20c66e5946c176b
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-19 14:14:07 -0400

    Enhance Quotes UI with methods to fetch quotes and user interaction buttons

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,9 +1,31 @@
 <script>
   export default {
-    name: 'App'
-  }
+    name: 'App',
+    data() {
+      return {
+        quoteId: ''
+      };
+    },
+    methods: {
+      fetchAllQuotes() {
+        console.log('Fetching all quotes...');
+      },
+      fetchRandomQuote() {
+        console.log('Fetching random quote...');
+      },
+      fetchQuoteById() {
+        console.log('Fetching quote with ID:', this.quoteId);
+      }
+    }
+  };
 </script>
 
 <template>
-    <div>Hello from Quotes UI!</div>
+  <div>
+    <h1>Quotes UI</h1>
+    <button @click="fetchAllQuotes">Get All Quotes</button>
+    <button @click="fetchRandomQuote">Get Random Quote</button>
+    <input v-model="quoteId" placeholder="Enter quote ID" />
+    <button @click="fetchQuoteById">Get Quote by ID</button>
+  </div>
 </template>
