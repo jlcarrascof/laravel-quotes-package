9364c55ed351e1bae2a60bdb84373589fd2bf74e
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-03-19 14:25:33 -0400

    Implement quote fetching in App.vue with error handling and display

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,23 @@
 <script>
+  import axios from 'axios';
+
   export default {
     name: 'App',
     data() {
       return {
-        quoteId: ''
+        quoteId: '',
+        quotes: []
       };
     },
     methods: {
-      fetchAllQuotes() {
-        console.log('Fetching all quotes...');
+      async fetchAllQuotes() {
+        try {
+          const response = await axios.get('/api/quotes');
+          this.quotes = response.data;
+        } catch (error) {
+          console.error('Error fetching quotes:', error);
+          this.quotes = [];
+        }
       },
       fetchRandomQuote() {
         console.log('Fetching random quote...');
@@ -27,5 +36,12 @@
     <button @click="fetchRandomQuote">Get Random Quote</button>
     <input v-model="quoteId" placeholder="Enter quote ID" />
     <button @click="fetchQuoteById">Get Quote by ID</button>
+
+    <ul v-if="quotes.length">
+      <li v-for="quote in quotes" :key="quote.id">
+        {{ quote.quote }} - {{ quote.author }}
+      </li>
+    </ul>
+    <p v-else>No quotes yet.</p>
   </div>
 </template>
